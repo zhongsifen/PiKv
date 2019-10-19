0c90bfa4679129626594d5a5da8092ec0055c20b
zhongsifen <zhongsifen@gmail.com>
2019-10-19 21:18:55 +0800

    error PiMien

--- PiMien/KvMien.py
+++ PiMien/KvMien.py
@@ -67,24 +67,6 @@ class KvMien(AnchorLayout):
     def KvFace(self, instance):
         pass
 
-    # def KvFace(self, instance):
-    #     dt = 1.0 / 25.0
-    #     self.KvFaceSetup()
-    #     Clock.schedule_interval(self.KvFaceRun, dt)
-
-    # def KvFaceSetup(self):
-    #     cam = self.ids._camera
-    #     Cim.open(cam.get_size(), cam.get_colorfmt())
-    #     Cim.read_rgba(cam.get_pixels())
-
-    # def KvFaceRun(self, dt):
-    #     size = Cim.size()
-    #     pixels = bytes((size[0]*size[1])*3)
-    #     Cim.write_rgb(pixels)
-    #     prv = self.ids._preview
-    #     prv.setup(size=size)
-    #     prv.show(pixels=pixels, size=size, colorfmt='rgb')
-
 
 class KvMienApp(App):
     def build(self):
--- PiMien/PiMien.py
+++ PiMien/PiMien.py
@@ -2,12 +2,10 @@
 
 import sys
 sys.path.append('/Users/zhongsifen/Work/PiKv')
-
 from PiCim.PiCim import Cim
 
 class PiMien:
     def Setup(self):
-        cam = self.ids._camera
         Cim.open(cam.get_size(), cam.get_colorfmt())
         Cim.read_rgba(cam.get_pixels())
 
@@ -18,3 +16,21 @@ class PiMien:
         prv = self.ids._preview
         prv.setup(size=size)
         prv.show(pixels=pixels, size=size, colorfmt='rgb')
+
+    # def KvFace(self, instance):
+    #     dt = 1.0 / 25.0
+    #     self.KvFaceSetup()
+    #     Clock.schedule_interval(self.KvFaceRun, dt)
+
+    # def KvFaceSetup(self):
+    #     cam = self.ids._camera
+    #     Cim.open(cam.get_size(), cam.get_colorfmt())
+    #     Cim.read_rgba(cam.get_pixels())
+
+    # def KvFaceRun(self, dt):
+    #     size = Cim.size()
+    #     pixels = bytes((size[0]*size[1])*3)
+    #     Cim.write_rgb(pixels)
+    #     prv = self.ids._preview
+    #     prv.setup(size=size)
+    #     prv.show(pixels=pixels, size=size, colorfmt='rgb')
